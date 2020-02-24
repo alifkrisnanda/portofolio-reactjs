b982423af5afdb92db8ec10fe5b20c9a7d42187a
alifkrisnanda <alifkrisnanda023@gmail.com>
2020-02-24 14:12:04 +0700

    portfolio

---
+++ build/precache-manifest.b26e5c7290b19ef03fd3165c38a7e0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd148d9b6101dea369a6d1e4fa7d7be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464002422bfb59f21c9c",
+    "url": "/static/css/main.74fdb1f7.chunk.css"
+  },
+  {
+    "revision": "575c709f671813e7f912",
+    "url": "/static/js/2.eab1dad7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.eab1dad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464002422bfb59f21c9c",
+    "url": "/static/js/main.77910438.chunk.js"
+  },
+  {
+    "revision": "2d115e4a6273f4311729",
+    "url": "/static/js/runtime-main.44b85020.js"
+  }
+]);
