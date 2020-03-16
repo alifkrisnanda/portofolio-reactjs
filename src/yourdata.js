b6a4d1612843fee332bec4b9545e25e2c4d8a137
alifkrisnanda <alifkrisnanda023@gmail.com>
2020-03-16 10:25:42 +0700

    refaktoring

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,69 +1,32 @@
 export default
     {
-        //(Please Do Not Remove The comma(,) after every variable)
-        //Change The Website Template
         name :'alifkrisnanda.',
-        headerTagline: [//Line 1 For Header
+        headerTagline: [
                         'Hi 👋 You found me! I am Alif Krisnanda.',
-                        //Line 2 For Header
                         'Junior Programmer ',
-                        //Line 3 For Header
                         'from Indonesia'
     ],
-        //Contact Email
         contactEmail:'alifkrisnanda023@gmail.com',
-        // Add Your About Text Here
         abouttext: "I'm student at The State of Vocational High School 1 Bawang, who ❤ </> and 🍔.",
         aboutImage:'https://images.unsplash.com/photo-1582517189042-fed4e613df2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1938&q=80',
-       //Change This To Hide The Image of About Section (True Or False)
-       ShowAboutImage:true,//true or false (Change Here)
-       // Change Projects Here 
+       ShowAboutImage:true,
        projects:[
            {
-            id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Internship at CV Prima', //Project Title - Add Your Project Title Here
-             service:'Design (1 month)', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
+            id: 1,
+            title:'Internship at CV Prima',
+             service:'Design (1 month)',
              imageSrc: "https://images.unsplash.com/photo-1582521063878-7748f3570d90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1047&q=80",
-             //Project URL - Add Your Project Url Here
              url:'https://github.com/alifkrisnanda'
             },
             {
-                id: 2,//DO NOT CHANGE THIS (Please)😅
+                id: 2,
                 title: 'Internship at Nusantech',
                 service: 'Website Development (1 year)',
                 imageSrc:"https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
                 url: 'https://github.com/alifkrisnanda'
             },
-        //     { 
-        //         id: 3,//DO NOT CHANGE THIS (Please)😅
-        //         title: 'Project Three',
-        //         service: 'Web App',
-        //         imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        //         url: 'http://chetanverma.com/'
-        //     },
-        //     {
-        //         id: 4,//DO NOT CHANGE THIS (Please)😅
-        //         title: 'Project Four',
-        //         service: 'Branding',
-        //         imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        //         url: 'http://chetanverma.com/'
-        //    }
-
-                    /*
-
-                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
-                ,{
-                id: 5,
-                title: 'Project Five',
-                service: 'Something Amazing',
-                imageSrc: "",
-                url: ''
-            }
-                */
         ],
         social: [
-            // Add Or Remove The Link Accordingly
             {   name:'Github',
                 url:'https://github.com/alifkrisnanda'},
             {
